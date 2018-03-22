44b60bb0eb88b8fdaeba4c338acf8e464b5d14aa
Nick Drishinski <ndrishinski@yahoo.com>
2018-03-22 14:48:19 -0600

    Implemented Angular Form

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
