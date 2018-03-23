849ba572ab6bc611765b22d35c9f9ee3c3ec5876
Nick Drishinski <ndrishinski@yahoo.com>
2018-03-23 15:21:58 -0600

    Added bootstrap cards for images

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -16,21 +16,21 @@ var DocumentsComponent = /** @class */ (function () {
                 description: "asdfasdf asdfasdf",
                 file_url: "http://google.com",
                 updated_at: '11/11/16',
-                image_url: 'http://google.com',
+                image_url: 'https://careerlancer.net/wp-content/uploads/2015/12/challenges-freelancer.jpg',
             },
             {
                 title: "My Second Doc",
                 description: "asdfasdf asdfasdf",
                 file_url: "http://google.com",
                 updated_at: '11/11/16',
-                image_url: 'http://google.com',
+                image_url: 'https://pakwired.com/wp-content/uploads/2017/10/Freelancer-No-credit.jpg',
             },
             {
                 title: "My Third Doc",
                 description: "asdfasdf asdfasdf",
                 file_url: "http://google.com",
                 updated_at: '11/11/16',
-                image_url: 'http://google.com',
+                image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUBJDSOTymv6AtVRUbBDuHfd7D7PdN72G3D57YQ2ebI8jNrIXt',
             }
         ];
     }
